699f211f557adf19a43d290b2bf2a8e3160f1784
Pavel Lebedev <maxwinrate@gmail.com>
2020-03-26 22:52:09 +0200

    Added footer.php

--- wp-content/themes/fictional-university-theme/index.php
+++ wp-content/themes/fictional-university-theme/index.php
@@ -1,4 +1,5 @@
 <?php
+get_header();
 while ( have_posts() ) {
 	the_post(); ?>
     <h1><a href="<?php the_permalink(); ?>"><?php the_title() ?></a></h1>
--- wp-content/themes/fictional-university-theme/page.php
+++ wp-content/themes/fictional-university-theme/page.php
@@ -1,4 +1,5 @@
 <?php declare( strict_types=1 );
+
 while ( have_posts() ) {
 	the_post(); ?>
 	<h1>This is a Page not A Post</h1>
