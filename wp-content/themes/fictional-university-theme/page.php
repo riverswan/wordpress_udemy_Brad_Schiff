564dcff000794b6ccfcc91adab00a4cda1a85760
Pavel Lebedev <maxwinrate@gmail.com>
2020-03-27 15:45:47 +0200

    page.php added breadcrumbs

--- wp-content/themes/fictional-university-theme/page.php
+++ wp-content/themes/fictional-university-theme/page.php
@@ -18,38 +18,58 @@ while ( have_posts() ) {
 
     <div class="container container--narrow page-section">
 
-		<?php
-		$theParent = wp_get_post_parent_id( get_the_ID() );
-		if ( $theParent !== 0 ) : ?>
-            <div class="metabox metabox--position-up metabox--with-home-link">
-                <p>
-                    <a class="metabox__blog-home-link" href="<?php echo get_permalink($theParent) ?>">
-                        <i class="fa fa-home" aria-hidden="true">
-                        </i> Back to <?php echo get_the_title($theParent) ?>
-                    </a>
-                    <span class="metabox__main">
+	<?php
+	$theParent = wp_get_post_parent_id( get_the_ID() );
+	if ( $theParent !== 0 ) : ?>
+        <div class="metabox metabox--position-up metabox--with-home-link">
+            <p>
+                <a class="metabox__blog-home-link" href="<?php echo get_permalink( $theParent ) ?>">
+                    <i class="fa fa-home" aria-hidden="true">
+                    </i> Back to <?php echo get_the_title( $theParent ) ?>
+                </a>
+                <span class="metabox__main">
                          <?php the_title() ?>
                      </span>
-                </p>
-            </div>
-		<?php endif;
-		?>
+            </p>
+        </div>
+	<?php endif;
+	?>
 
+	<?php
+	$testArray = get_pages(
+		array(
+			'child_of' => get_the_ID()
+		)
+	);
+	if ( $theParent or $testArray ) : ?>
 
-        <!--        <div class="page-links">-->
-        <!--            <h2 class="page-links__title"><a href="#">About Us</a></h2>-->
-        <!--            <ul class="min-list">-->
-        <!--                <li class="current_page_item"><a href="#">Our History</a></li>-->
-        <!--                <li><a href="#">Our Goals</a></li>-->
-        <!--            </ul>-->
-        <!--        </div>-->
+        <div class="page-links">
+            <h2 class="page-links__title"><a
+                        href="<?php echo get_permalink( $theParent ) ?>"><?php echo get_the_title( $theParent ) ?></a>
+            </h2>
+            <ul class="min-list">
+				<?php
+				$findChildrenOf = 0;
+				if ( $theParent !== 0 ) {
+					$findChildrenOf = $theParent;
+				} else {
+					$findChildrenOf = get_the_ID();
+				}
+				$args = array(
+					'title_li' => null,
+					'child_of' => $findChildrenOf
+				);
+				wp_list_pages( $args );
+				?>
+            </ul>
+        </div>
 
         <div class="generic-content">
 			<?php the_content(); ?>
         </div>
 
-    </div>
+        </div>
 
-	<?php
+	<?php endif;
 }
 get_footer();
