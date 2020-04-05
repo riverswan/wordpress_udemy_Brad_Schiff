62bfde8435d4f211f19f709340a3f6de933a5ec4
Pavel Lebedev <maxwinrate@gmail.com>
2020-04-05 19:56:27 +0300

    add_action( 'pre_get_posts')

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -20,8 +20,19 @@ add_action( 'after_setup_theme', 'university_features' );
 
 
 function university_adjust_queries( $query ) {
-	if ( ! is_admin() ) {
-		$query->set( '', '' );
+	if ( ! is_admin() && is_post_type_archive( 'event' ) && $query->is_main_query() ) {
+		$today = \date('Ymd');
+		$query->set( 'meta_key', 'event_date' );
+		$query->set( 'orderby', 'meta_value_num' );
+		$query->set( 'order', 'ASC' );
+		$query->set( 'meta_query', array(
+			array(
+				'key'     => 'event_date',
+				'compare' => '>=',
+				'value'   => $today,
+				'type'    => 'numeric'
+			)
+		) );
 	}
 }
 
