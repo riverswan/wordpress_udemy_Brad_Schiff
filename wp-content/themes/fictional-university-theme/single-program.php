f46b6fc469b21141bef7ad0900fc1ec35ab95bee
Pavel Lebedev <maxwinrate@gmail.com>
2020-04-20 19:41:45 +0300

    Reformat

--- wp-content/themes/fictional-university-theme/single-program.php
+++ wp-content/themes/fictional-university-theme/single-program.php
@@ -3,33 +3,33 @@ get_header();
 while ( have_posts() ) {
 	the_post(); ?>
 
-	<div class="page-banner">
-		<div class="page-banner__bg-image"
-		     style="background-image: url(<?php echo get_theme_file_uri( '/images/ocean.jpg' ) ?>);"></div>
-		<div class="page-banner__content container container--narrow">
-			<h1 class="page-banner__title">
+    <div class="page-banner">
+        <div class="page-banner__bg-image"
+             style="background-image: url(<?php echo get_theme_file_uri( '/images/ocean.jpg' ) ?>);"></div>
+        <div class="page-banner__content container container--narrow">
+            <h1 class="page-banner__title">
 				<?php the_title() ?>
-			</h1>
-			<div class="page-banner__intro">
+            </h1>
+            <div class="page-banner__intro">
 				<?php echo 'DON\'T FORGET TO REPLACE ME LATER' ?>
-			</div>
-		</div>
-	</div>
-	<div class="container container--narrow page-section">
-		<div class="metabox metabox--position-up metabox--with-home-link">
-			<p>
-				<a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('program')?>">
-					<i class="fa fa-home" aria-hidden="true">
-					</i> All Programs
-				</a>
-				<span class="metabox__main">
+            </div>
+        </div>
+    </div>
+    <div class="container container--narrow page-section">
+        <div class="metabox metabox--position-up metabox--with-home-link">
+            <p>
+                <a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link( 'program' ) ?>">
+                    <i class="fa fa-home" aria-hidden="true">
+                    </i> All Programs
+                </a>
+                <span class="metabox__main">
 					<?php the_title() ?>
                      </span>
-			</p>
-		</div>
-		<div class="generic-content"><?php the_content(); ?></div>
+            </p>
+        </div>
+        <div class="generic-content"><?php the_content(); ?></div>
 		<?php
-		$today = \date('Ymd');
+		$today          = date( 'Ymd' );
 		$homePageEvents = new WP_Query( array(
 			'post_type'      => 'event',
 			'posts_per_page' => 2,
@@ -46,7 +46,7 @@ while ( have_posts() ) {
 				array(
 					'key'     => 'related_programs',
 					'compare' => 'LIKE',
-					'value'   => '"'.get_the_ID().'"',
+					'value'   => '"' . get_the_ID() . '"',
 				)
 			)
 		) );
@@ -54,30 +54,31 @@ while ( have_posts() ) {
 		while ( $homePageEvents->have_posts() ) {
 			$homePageEvents->the_post();
 			?>
-			<div class="event-summary">
-				<a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
+            <div class="event-summary">
+                <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
                             <span class="event-summary__month"><?php
 	                            $eventDate = new DateTime( get_field( 'event_date' ) );
 	                            echo $eventDate->format( 'M' );
 	                            ?></span>
-					<span class="event-summary__day"><?php echo $eventDate->format( 'd' ); ?></span>
-				</a>
-				<div class="event-summary__content">
-					<h5 class="event-summary__title headline headline--tiny"><a
-							href="<?php the_permalink(); ?>"><?php the_title() ?></a></h5>
-					<p><?php
+                    <span class="event-summary__day"><?php echo $eventDate->format( 'd' ); ?></span>
+                </a>
+                <div class="event-summary__content">
+                    <h5 class="event-summary__title headline headline--tiny"><a
+                                href="<?php the_permalink(); ?>"><?php the_title() ?></a></h5>
+                    <p><?php
 						if ( has_excerpt() ) {
 							echo get_the_excerpt();
 						} else {
 							echo wp_trim_words( get_the_content(), 10 );
 						}
 						?>
-						<a href="<?php get_the_permalink() ?>" class="nu gray">Learn more</a></p>
-				</div>
-			</div>
+                        <a href="<?php get_the_permalink() ?>" class="nu gray">Learn more</a></p>
+                </div>
+            </div>
 		<?php }
 		wp_reset_postdata();
 		?>
-	</div>
+    </div>
 
-<?php } get_footer();
+<?php }
+get_footer();
