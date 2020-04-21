a02fc14d519b9d2722ce24f6334c947ed347730f
Pavel Lebedev <maxwinrate@gmail.com>
2020-04-21 19:53:30 +0300

    Refactoring

--- wp-content/themes/fictional-university-theme/header.php
+++ wp-content/themes/fictional-university-theme/header.php
@@ -1,21 +1,23 @@
 <!doctype html>
-<html <?php language_attributes();?>>
+<html <?php language_attributes(); ?>>
 <head>
-    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale = 1">
-    <?php wp_head(); ?>
+	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
 <header class="site-header">
     <div class="container">
-        <h1 class="school-logo-text float-left"><a href="<?php echo site_url() ?>"><strong>Fictional</strong> University</a></h1>
-        <span class="js-search-trigger site-header__search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
+        <h1 class="school-logo-text float-left"><a href="<?php echo site_url() ?>"><strong>Fictional</strong> University</a>
+        </h1>
+        <span class="js-search-trigger site-header__search-trigger"><i class="fa fa-search"
+                                                                       aria-hidden="true"></i></span>
         <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
         <div class="site-header__menu group">
             <nav class="main-navigation">
-                <?php wp_nav_menu(array(
-                        'theme_location' => 'header_menu_location'
-                )) ?>
+				<?php wp_nav_menu( array(
+					'theme_location' => 'header_menu_location'
+				) ) ?>
 
             </nav>
             <div class="site-header__util">
