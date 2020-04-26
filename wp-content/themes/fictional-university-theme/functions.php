f433b9fc9805dcad21e953164f84d552f58b8f2f
Pavel Lebedev <maxwinrate@gmail.com>
2020-04-26 20:02:13 +0300

    Page banner func

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -59,3 +59,30 @@ function special_nav_class( $classes, $item ) {
 
 	return $classes;
 }
+
+?>
+
+
+<?php
+function page_banner($args){
+	if ( ! $args['title'] ){
+		$args['title'] = get_the_title();
+	}
+	?>
+	<div class="page-banner">
+		<div class="page-banner__bg-image"
+		     style="background-image: url(<?php
+		     $page_baner_image = get_field('page_banner_background_image');
+		     echo $page_baner_image['sizes']['page_banner']
+		     ?>);"></div>
+		<div class="page-banner__content container container--narrow">
+			<h1 class="page-banner__title">
+				<?php echo $args['title']?>
+			</h1>
+			<div class="page-banner__intro">
+				<?php the_field('page_banner_subtitle'); ?>
+			</div>
+		</div>
+	</div>
+<?php
+}
--- wp-content/themes/fictional-university-theme/single-professor.php
+++ wp-content/themes/fictional-university-theme/single-professor.php
@@ -3,29 +3,22 @@ get_header();
 while ( have_posts() ) {
 	the_post(); ?>
 
-	<div class="page-banner">
-		<div class="page-banner__bg-image"
-		     style="background-image: url(<?php
-             $page_baner_image = get_field('page_banner_background_image');
-             echo $page_baner_image['sizes']['page_banner']
-             ?>);"></div>
-		<div class="page-banner__content container container--narrow">
-			<h1 class="page-banner__title">
-				<?php the_title() ?>
-			</h1>
-			<div class="page-banner__intro">
-				<?php the_field('page_banner_subtitle'); ?>
-			</div>
-		</div>
-	</div>
-	<div class="container container--narrow page-section">
+	<?php echo page_banner(
+		array(
+			'title'    => 'Hello there',
+			'subtitle' => 'Subtitle',
+            'bgimage'  => ''
+		)
+	) ?>
 
-		<div class="generic-content">
-			<div class="row group">
-				<div class="one-third"><?php the_post_thumbnail('professor_portrait'); ?></div>
-				<div class="two-thirds"><?php the_content(); ?></div>
-			</div>
-		</div>
+    <div class="container container--narrow page-section">
+
+        <div class="generic-content">
+            <div class="row group">
+                <div class="one-third"><?php the_post_thumbnail( 'professor_portrait' ); ?></div>
+                <div class="two-thirds"><?php the_content(); ?></div>
+            </div>
+        </div>
 
 		<?php
 		$related_programs = get_field( 'related_programs' );
@@ -36,16 +29,16 @@ while ( have_posts() ) {
 			echo '<ul class="link-list min-list">';
 			foreach ( $related_programs as $related_program ) { ?>
 
-				<li>
-					<a href="<?php echo get_the_permalink( $related_program ) ?>"><?php echo get_the_title( $related_program ) ?></a>
-				</li>
+                <li>
+                    <a href="<?php echo get_the_permalink( $related_program ) ?>"><?php echo get_the_title( $related_program ) ?></a>
+                </li>
 
 				<?php
 			}
 			echo '</ul>';
 		}
 		?>
-	</div>
+    </div>
 
 <?php }
 get_footer();
