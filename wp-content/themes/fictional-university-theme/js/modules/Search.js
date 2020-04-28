1e7669f89a1ba9f7929b990cae07c75063799654
Pavel Lebedev <maxwinrate@gmail.com>
2020-04-28 18:38:59 +0300

    Search.js

--- wp-content/themes/fictional-university-theme/js/modules/Search.js
+++ wp-content/themes/fictional-university-theme/js/modules/Search.js
@@ -1,7 +1,20 @@
-class Search {
-    constructor() {
+import $ from 'jquery';
 
+class Search {
+
+    constructor() {
+        this.openButtom = $('.js-search-trigger');
+        this.closeButton = $('.search-overlay__close');
+        this.searchOverlay = $('.search-overlay');
     }
+
+    events(){
+      this.openButtom.on('click',this.openOverlay);
+      this.closeButton.on('click',this.closeOverlay)
+    };
+
+    openOverlay(){};
+    closeOverlay(){};
 }
 
 
