7812d785d6f3848a0ffcd7026c81114d59c45681
Pavel Lebedev <maxwinrate@gmail.com>
2020-04-30 12:04:35 +0300

    Async results

--- wp-content/themes/fictional-university-theme/js/modules/Search.js
+++ wp-content/themes/fictional-university-theme/js/modules/Search.js
@@ -43,19 +43,30 @@ class Search {
     }
 
     getResults() {
-        $.getJSON(
-            universityData.root_url + '/wp-json/wp/v2/posts?search=' + this.searchField.val(),
-            (posts) => {
 
-                this.resultsDiv.html(
-                    `
+        $.when(
+
+        ).then(
+            (data)=>{
+
+            }
+        );
+
+        $.getJSON(universityData.root_url + '/wp-json/wp/v2/posts?search=' + this.searchField.val(),
+            (posts) => {
+                $.getJSON(universityData.root_url + '/wp-json/wp/v2/pages?search=' + this.searchField.val(),(pages)=>{
+                    let combinedRes = posts.concat(pages);
+                    console.log(combinedRes);
+                    this.resultsDiv.html(
+                        `
                     <h2 class="search-overlay__section-title">General Information</h2>
-                    ${posts.length ? `<ul class="link-list min-list">` : `<p>No general info matches search</p>`}
-                     ${ posts.map((item)=>`<li><a href="${item.link}">${item.title.rendered}</a></li>`).join('') }
-                    ${ posts.length ? `</ul>` : ``}
+                    ${combinedRes.length ? `<ul class="link-list min-list">` : `<p>No general info matches search</p>`}
+                     ${ combinedRes.map((item)=>`<li><a href="${item.link}">${item.title.rendered}</a></li>`).join('') }
+                    ${ combinedRes.length ? `</ul>` : ``}
                     `
-                );
-                this.isSpinnerVisible = false;
+                    );
+                    this.isSpinnerVisible = false;
+                })
             },
         )
     }
