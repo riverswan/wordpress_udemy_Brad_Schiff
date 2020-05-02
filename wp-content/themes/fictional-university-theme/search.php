a4e3fc1818114dfa4aa056c6ba4ff8e200e44d72
Pavel Lebedev <maxwinrate@gmail.com>
2020-05-02 17:12:15 +0300

    HTML search stylizing

--- wp-content/themes/fictional-university-theme/search.php
+++ wp-content/themes/fictional-university-theme/search.php
@@ -1,26 +1,30 @@
 <?php
+
 get_header();
 page_banner(array(
 	'title' => 'Search Results',
-	'subtitle' => 'You searched for &ldquo;'. esc_html( get_search_query(false) ) .'&rdquo;'
+	'subtitle' => 'You searched for &ldquo;' . esc_html(get_search_query(false))  . '&rdquo;'
 ));
 ?>
 
-	<div class="container container--narrow page-section">
-		<?php while ( have_posts() ) {
-			the_post();
-			get_template_part();
-			?>
-			<div class="post-item">
-				<h2 class="headline headline--medium headline--post-title"><a href="<?php the_permalink(); ?>"><?php the_title() ?></a></h2>
-				<div class="metabox">
-					<p>Posted by:<?php the_author_posts_link();?> on: <?php the_time('n.j.y');?> in <?php echo get_the_category_list(', ') ?></p>
-				</div>
-				<div class="generic-content"><?php the_excerpt(); ?></div>
-				<p><a class="btn--blue btn" href="<?php the_permalink(); ?>">Continue reading &raquo;</a></p>
-			</div>
-		<?php }
-		echo paginate_links();
+    <div class="container container--narrow page-section">
+		<?php
+		if (have_posts()) {
+			while(have_posts()) {
+				the_post();
+				get_template_part('template-parts/content', get_post_type());
+			}
+			echo paginate_links();
+		} else {
+			echo '<h2 class="headline headline--small-plus">No results match that search.</h2>';
+		}
+
+		get_search_form();
+
 		?>
-	</div>
+
+    </div>
+
 <?php get_footer();
+
+?>
