046cdd96fc7b74a35975da12e788a434cd8e4afe
Pavel Lebedev <maxwinrate@gmail.com>
2020-05-06 17:05:18 +0300

    Custom note post type

--- wp-content/themes/fictional-university-theme/page-my-notes.php
+++ wp-content/themes/fictional-university-theme/page-my-notes.php
@@ -1,19 +1,27 @@
 <?php declare( strict_types=1 );
-if (!is_user_logged_in()){
-	wp_redirect( site_url('/') );
+if ( ! is_user_logged_in() ) {
+	wp_redirect( site_url( '/' ) );
 	exit();
 }
 get_header();
 while ( have_posts() ) {
 	the_post();
-	page_banner(array(
+	page_banner( array(
 		'title' => 'Notes'
-	));
+	) );
 	?>
 
-	<div class="container container--narrow page-section">
-
-	</div>
+    <div class="container container--narrow page-section">
+        <ul class="min-list link-list" id="mynotes">
+			<?php
+			$user_notes = new WP_Query( array(
+				'post_type'      => 'note',
+				'posts_per_page' => - 1,
+				'author'         => get_current_user_id()
+			) )
+			?>
+        </ul>
+    </div>
 
 <?php }
 
