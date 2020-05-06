eb905b7940c51c6702b65f15e44c1e6731545252
Pavel Lebedev <maxwinrate@gmail.com>
2020-05-06 17:10:45 +0300

    Custom note post type buttons

--- wp-content/themes/fictional-university-theme/page-my-notes.php
+++ wp-content/themes/fictional-university-theme/page-my-notes.php
@@ -18,7 +18,20 @@ while ( have_posts() ) {
 				'post_type'      => 'note',
 				'posts_per_page' => - 1,
 				'author'         => get_current_user_id()
-			) )
+			) );
+
+			while ( $user_notes->have_posts() ) {
+				$user_notes->the_post(); ?>
+
+                <li>
+                    <input class="note-title-field" type="text" value="<?php echo esc_attr( get_the_title() ) ?>">
+                    <span class="edit-note"><i class="fa fa-pencil"></i>Edit</span>
+                    <span class="delete-note"><i class="fa fa-trash-o"></i>Delete</span>
+                    <textarea class="note-body-field" name="" id="" cols="30" rows="10"><?php echo esc_attr( get_the_content() ) ?></textarea>
+                </li>
+
+                <?php
+			}
 			?>
         </ul>
     </div>
