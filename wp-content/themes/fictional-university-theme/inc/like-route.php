8f62f1e7fb7bf2bd348bf4ed4d7c1961cae8f76e
Pavel Lebedev <maxwinrate@gmail.com>
2020-05-08 19:58:15 +0300

    Like route

--- wp-content/themes/fictional-university-theme/inc/like-route.php
+++ wp-content/themes/fictional-university-theme/inc/like-route.php
@@ -1,3 +1,24 @@
 <?php declare( strict_types=1 );
 
 
+add_action('rest_api_init','university_like_routes');
+
+function university_like_routes(){
+	register_rest_route('university/v1','manageLike',array(
+		'methods' => WP_REST_Server::CREATABLE,
+		'callback' => 'create_like'
+	));
+
+	register_rest_route('university/v1','manageLike',array(
+		'methods' => WP_REST_Server::DELETABLE,
+		'callback' => 'delete_like'
+	));
+}
+
+function create_like(){
+	return 'like created';
+}
+
+function delete_like(){
+	return 'like deleted';
+}
--- wp-content/themes/fictional-university-theme/js/modules/Like.js
+++ wp-content/themes/fictional-university-theme/js/modules/Like.js
@@ -19,7 +19,7 @@ class Like {
     }
 
     createLike(){
-        alert('create')
+
     }
 
     deleteLike(){
